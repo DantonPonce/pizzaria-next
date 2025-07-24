b069ea3a1d7379f5c8f47c3a26596086c6a33ce1
Danton Ponce <dantonponce@MacBook-Air-de-Danton.local>
2025-07-24 00:59:15 -0300

    feat: finishing order

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -6,6 +6,8 @@ import { OrdersProps } from "@/lib/order.type";
 import { ModalOrder } from "../modal";
 import { OrderContext } from "@/providers/order";
 import { use } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface Props {
   orders: OrdersProps[];
@@ -13,9 +15,15 @@ interface Props {
 
 export function Orders({ orders }: Props) {
   const { isOpen, onRequestOpen } = use(OrderContext);
+  const router = useRouter();
 
-  function handleDetailOrder(order_id: string){
-    onRequestOpen(order_id);
+  async function handleDetailOrder(order_id: string) {
+    await onRequestOpen(order_id);
+  }
+
+  function handleRefresh() {
+    router.refresh();
+    toast.success("Lista de pedidos atualizada com sucesso!");
   }
 
   return (
@@ -23,14 +31,24 @@ export function Orders({ orders }: Props) {
       <main className={styles.container}>
         <section className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCw size={24} color="#3fffa3" />
           </button>
         </section>
 
         <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <div className={styles.emptyItem}>
+              <p>Nenhum pedido encontrado no momento...</p>
+            </div>
+          )}
+
           {orders.map((order) => (
-            <button key={order.id} className={styles.orderItem} onClick={() => handleDetailOrder(order.id)}>
+            <button
+              key={order.id}
+              className={styles.orderItem}
+              onClick={() => handleDetailOrder(order.id)}
+            >
               <div className={styles.tag}></div>
               <span>Mesa {order.table}</span>
             </button>
--- src/providers/order.tsx
+++ src/providers/order.tsx
@@ -2,40 +2,44 @@
 
 import { getCookieClient } from "@/lib/cookieClient";
 import { api } from "@/services/api";
+import { useRouter } from "next/navigation";
 import { createContext, ReactNode, useState } from "react";
+import { toast } from "sonner";
 
 export interface OrderItemProps {
-  id:         string;
-  amount:     number;
+  id: string;
+  amount: number;
   created_at: Date;
   updated_at: Date;
-  order_id:   string;
+  order_id: string;
   product_id: string;
-  product:    Product;
-  order:      Order;
+  product: Product;
+  order: Order;
 }
 
 export interface Order {
-  id:         string;
-  table:      number;
-  status:     boolean;
-  draft:      boolean;
-  name:       string;
+  id: string;
+  table: number;
+  status: boolean;
+  draft: boolean;
+  name?: string;
 }
 
 export interface Product {
-  id:          string;
-  name:        string;
-  price:       string;
+  id: string;
+  name: string;
+  price: string;
   description: string;
-  banner:      string;
+  banner: string;
   category_id: string;
 }
 
 type OrderContextData = {
   isOpen: boolean;
-  onRequestOpen: (order_id: string) => void;
+  onRequestOpen: (order_id: string) => Promise<void>;
   onRequestClose: () => void;
+  order: OrderItemProps[];
+  finishOrder: (order_id: string) => Promise<void>;
 };
 
 type OrderProviderProps = {
@@ -46,27 +50,50 @@ export const OrderContext = createContext({} as OrderContextData);
 
 export function OrderProvider({ children }: OrderProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [order, setOrder] = useState<OrderItemProps[]>();
+  const [order, setOrder] = useState<OrderItemProps[]>([]);
+  const router = useRouter();
 
   async function onRequestOpen(order_id: string) {
-    setIsOpen(true);
-
     const token = await getCookieClient();
 
     const response = await api.get("/order/detail", {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
-      params:{
-        order_id: order_id
-      }
+      params: {
+        order_id: order_id,
+      },
     });
 
-    console.log(response.data);
-    
+    setOrder(response.data);
+    setIsOpen(true);
   }
 
   function onRequestClose() {
+    setIsOpen(false);
+  }
+
+  async function finishOrder(order_id: string) {
+    const token = await getCookieClient();
+
+    const data = {
+      order_id: order_id,
+    };
+
+    try {
+      await api.put("order/finish", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Falaha ao finalizar este pedido!");
+      return;
+    }
+
+    toast.success("Pedido concluído com sucesso!");
+    router.refresh();
     setIsOpen(false);
   }
 
@@ -76,6 +103,8 @@ export function OrderProvider({ children }: OrderProviderProps) {
         isOpen,
         onRequestOpen,
         onRequestClose,
+        order,
+        finishOrder
       }}
     >
       {children}
